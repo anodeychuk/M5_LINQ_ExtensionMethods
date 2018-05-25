c0c28dae6864f922328383caeb0af0daeaa0fa8d
Andrii <anodeychuk@gmail.com>
2018-05-25 15:05:38 +0300

    Add project files.

---
+++ M5_LINQ/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M5_LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
